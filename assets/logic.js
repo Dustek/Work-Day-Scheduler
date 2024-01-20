f7d37e5d98d0e1898196130a03c9f9664d33fb75
Vytas <v.venckas@gmail.com>
2024-01-20 11:11:39 +0000

    made time blocks update based on current hour

--- assets/logic.js
+++ assets/logic.js
@@ -6,3 +6,25 @@ dayjs.extend(window.dayjs_plugin_advancedFormat);
 var now = dayjs()
 var currentDate = now.format("dddd MMMM [the] Do")
 $("#currentDay").text(currentDate)
+
+
+// gets current time
+var currentTime = dayjs().format('HH');
+
+
+$(".time-block").each(function() {
+
+ var blockHour =  $(this).data("hour");
+
+
+if (blockHour > currentTime) {
+    $(this).find("textarea").removeClass("past present").addClass("future");
+}
+else if (blockHour == currentTime) {
+    $(this).find("textarea").removeClass("past future").addClass("present");
+    }
+else if (blockHour < currentTime) {
+    $(this).find("textarea").removeClass("present future").addClass("past");
+}
+})
+
